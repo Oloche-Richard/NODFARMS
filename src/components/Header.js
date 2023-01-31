ffcbebbc9215e4e147d8fd5a042551d9aeeb0b0e
Oloche Richard <98244432+Oloche-Richard@users.noreply.github.com>
2023-01-31 21:54:42 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,33 @@ import {StyleSheet, View, Text, TextInput, } from 'react-native'
 
 export default function Header(){
   return(
-    <View style={styles.Header}>
-    <View >
+    <View style={styles.headerContainer}>
+    <View style={styles.header}>
+    <View>
       <Text style={styles.headerText}>NODFARMS</Text>
     </View>
+    <View>
+            <Ionicons name="contact" size={22} color='gold'></Ionicons>
+            <Ionicons name="contact" size={22} color='gold'></Ionicons>
+            <Ionicons name="contact" size={22} color='gold'></Ionicons>
+    </View>
+    </View >
     </View>
   )
 } 
 
 const styles= StyleSheet.create({
-  Header: {
+  headerContainer: {
     backgroundColor: 'green',
     padding: 20,
     width: '100%'
+  },
+  header: {
+    flex:1,
+    flexDirection:'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingRight:20
   },
   headerText:{
     color: '#fff',
